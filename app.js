e31dfda26923279f6427b0f31ced65bb841e7ed6
Kavan <kavan155gondalia@gmail.com>
2022-10-12 16:45:27 +0530

    feat: counter animation in footer figures

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const darkBtn = document.getElementById('checkbox');
+$.fn.jQuerySimpleCounter = function( options ) {
+  var settings = $.extend({
+      start:  0,
+      end:    100,
+      easing: 'swing',
+      duration: 400,
+      complete: ''
+  }, options );
 
-darkBtn.addEventListener('change', ()=>{
-  document.body.classList.toggle('dark');
-})
+  var thisElement = $(this);
+
+  $({count: settings.start}).animate({count: settings.end}, {
+  duration: settings.duration,
+  easing: settings.easing,
+  step: function() {
+    var mathCount = Math.ceil(this.count);
+    thisElement.text(mathCount);
+  },
+  complete: settings.complete
+});
+};
+
+
+$('.counter1').jQuerySimpleCounter({end: 12,duration: 3000});
+$('.counter2').jQuerySimpleCounter({end: 55,duration: 3000});
+$('.counter3').jQuerySimpleCounter({end: 359,duration: 2000});
+$('.counter4').jQuerySimpleCounter({end: 246,duration: 2500});
+$('.counter5').jQuerySimpleCounter({end: 246,duration: 2500});
